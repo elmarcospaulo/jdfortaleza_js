02c9f410b22e9f03c1eceffdeb1aaa3979f8cd66
Marcos Paulo <109634856+elmarcospaulo@users.noreply.github.com>
2022-07-30 23:42:31 -0300

    Add files via upload

---
+++ TESTE CONDICIONAL II/EXERCICIO1/script.js
@@ -0,0 +1,8 @@
+var n1 = parseInt(prompt("Digite um número:"));
+
+if (n1 % 2 = 0) {
+
+	alert(`O número ${n1} é: par`);
+} else {
+	alert(`O número ${n1} é: ímpar`);
+}
---
+++ TESTE CONDICIONAL II/script.js
@@ -0,0 +1,11 @@
+var n = prompt("Digite um número");
+var resultado = n % 2;
+
+if (n % 2 = 2) {
+	alert(`O ${n} é um número par`);
+}
+
+else if (n % 2 = 1) {
+
+	alert(`O ${n} é um número ímpar`);
+}
